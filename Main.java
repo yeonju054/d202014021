5edc1fa9c8adc0f4f9102a68b704fc83336aa82e
Yu,yeonju <yeonju010504@gmail.com>
2022-09-26 15:15:05 +0900

    Main.java 수정 #2

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main { 
-    public static void main(String[] args) { 
-        String s = "hello world"; 
+    static void say(String s) { 
         System.out.println(s); 
+    } 
+    public static void main(String[] args) {
+        say("hello world"); 
     } 
 }
